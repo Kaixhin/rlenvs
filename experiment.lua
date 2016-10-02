1fe33fae4e476902eb98713f1d5b695d1f32086e
SeanNaren <seannaren@hotmail.com>
2016-10-02 13:14:09 +0100

    Experiment uses new specs

--- experiment.lua
+++ experiment.lua
@@ -6,33 +6,32 @@ local qt = pcall(require, 'qt')
 
 -- Initialise and start environment
 local env = Catch({level = 2})
-local stateSpec = env:stateSpace()
-local actionSpec = env:actionSpace()
+local actionSpace = env:actionSpace()
 local observation = env:start()
 
-local reward, terminal
+local reward, terminal = 0, false
 local episodes, totalReward = 0, 0
-local nSteps = 1000 * (stateSpec[2][2] - 1) -- Run for 1000 episodes
+local nEpisodes = 1000
 
 -- Display
 local window = qt and image.display({image=observation, zoom=10})
 
-for i = 1, nSteps do
-  -- Pick random action and execute it
-  local action = torch.random(actionSpec[3][1], actionSpec[3][2])
-  reward, observation, terminal = env:step(action)
-  totalReward = totalReward + reward
+for i = 1, nEpisodes do
+  while not terminal do
+    -- Pick random action and execute it
+    local action = torch.random(0, actionSpace['n'] - 1)
+    reward, observation, terminal = env:step(action)
+    totalReward = totalReward + reward
 
-  -- Display
-  if qt then
-    image.display({image=observation, zoom=10, win=window})
+    -- Display
+    if qt then
+        image.display({image=observation, zoom=10, win=window})
+    end
   end
 
-  -- If game finished, start again
-  if terminal then
-    episodes = episodes + 1
-    observation = env:start()
-  end
+  episodes = episodes + 1
+  observation = env:start()
+  terminal = false
 end
 print('Episodes: ' .. episodes)
 print('Total Reward: ' .. totalReward)

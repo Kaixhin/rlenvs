ee4a6b0cd89726eba1753e818ec10b5d5b7056e7
SeanNaren <seannaren@hotmail.com>
2016-11-28 18:06:28 +0000

    Test uses 2 space indentation

--- tests/test.lua
+++ tests/test.lua
@@ -5,35 +5,35 @@ local test = torch.TestSuite()
 local tester
 
 function test.envs()
-    for index, env in ipairs(rlenvs.envs) do
-        local function runTest()
-            local Env = require('rlenvs.' .. env)
-            -- Initialise and start environment
-            local env = Env()
-            local actionSpace = env:getActionSpace()
-            local observation = env:start()
-            -- Pick random action and execute it
-            local action = torch.random(0, actionSpace['n'] - 1)
-            local reward, observation, terminal = env:step(action)
-            -- Display if implemented
-            env:render()
-        end
-
-        if env == 'Atari' then
-            local hasALEWrap = pcall(require, 'alewrap')
-            if not hasALEWrap then
-                tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
-            end
-        elseif env == 'Minecraft' then
-            local hasSocket = pcall(require, 'socket')
-            local hasLibMalmoLua = pcall(require, 'libMalmoLua')
-            if not hasSocket and hasLibMalmoLua then
-                tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
-            end
-        else
-            tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
-        end
+  for index, env in ipairs(rlenvs.envs) do
+    local function runTest()
+      local Env = require('rlenvs.' .. env)
+      -- Initialise and start environment
+      local env = Env()
+      local actionSpace = env:getActionSpace()
+      local observation = env:start()
+      -- Pick random action and execute it
+      local action = torch.random(0, actionSpace['n'] - 1)
+      local reward, observation, terminal = env:step(action)
+      -- Display if implemented
+      env:render()
     end
+
+    if env == 'Atari' then
+      local hasALEWrap = pcall(require, 'alewrap')
+      if not hasALEWrap then
+        tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
+      end
+    elseif env == 'Minecraft' then
+      local hasSocket = pcall(require, 'socket')
+      local hasLibMalmoLua = pcall(require, 'libMalmoLua')
+      if not hasSocket and hasLibMalmoLua then
+        tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
+      end
+    else
+      tester:assert(pcall(runTest), 'Failed to run rlenv environment ' .. env)
+    end
+  end
 end
 
 tester = torch.Tester()
